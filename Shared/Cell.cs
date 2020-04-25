744d68e8c81a306cfb25dcb1d4bb54aba35d9483
Christian <christianle87@icloud.com>
2020-04-25 18:00:31 +0200

    control if dead or alive

--- Shared/Cell.cs
+++ Shared/Cell.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Shared
 {
@@ -8,32 +9,54 @@ namespace Shared
     {
         Texture2D aliveTexture;
         Texture2D deadTexture;
-        Vector2 position;
+        Rectangle rectangle;
 
         public bool isAlive;
 
-        public Cell(Vector2 position, bool isAlive, Color aliveColor, Color deadColor)
+        public Cell(Rectangle rectangle, bool isAlive, Color aliveColor, Color deadColor)
         {
-            this.position = position;
+            this.rectangle = rectangle;
             this.isAlive = isAlive;
 
-            this.aliveTexture = Tools.CreateColorTexture(aliveColor, 10, 10);
-            this.deadTexture = Tools.CreateColorTexture(deadColor, 10, 10);
+            this.aliveTexture = Tools.CreateColorTexture(aliveColor, rectangle.Width, rectangle.Height);
+            this.deadTexture = Tools.CreateColorTexture(deadColor, rectangle.Width, rectangle.Height);
         }
 
         internal void Update()
         {
+            MouseState mouseState = Mouse.GetState();
+
+            int mousePosX = mouseState.Position.X;
+            int mousePosY = mouseState.Position.Y;
+
+            if (
+                    mousePosX < rectangle.X + 10 &&
+                    mousePosX > rectangle.X &&
+                    mousePosY < rectangle.Y + 10 &&
+                    mousePosY > rectangle.Y
+                )
+            {
+                if (mouseState.RightButton == ButtonState.Pressed)
+                {
+                    isAlive = true;
+                }
+                else if(mouseState.LeftButton == ButtonState.Pressed)
+                {
+                    isAlive = false;
+                }
+            }
+
         }
 
         internal void Draw(SpriteBatch spriteBatch)
         {
             if (isAlive)
             {
-                spriteBatch.Draw(aliveTexture, position, Color.White);
+                spriteBatch.Draw(aliveTexture, rectangle, Color.White);
             }
             else
             {
-                spriteBatch.Draw(deadTexture, position, Color.White);
+                spriteBatch.Draw(deadTexture, rectangle, Color.White);
             }
         }
     }
--- Shared/Scene1.cs
+++ Shared/Scene1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Shared
 {
@@ -10,26 +11,28 @@ namespace Shared
         Cell[,] cell = new Cell[50, 50];
         public Scene1(ContentManager content)
         {
-            for(var row= 0;row<50; row++)
+            for (var row = 0; row < 50; row++)
             {
                 for (var col = 0; col < 50; col++)
                 {
-                    cell[row, col] = new Cell(new Vector2(col * 10, row * 10), true, Color.Green, Color.Brown);
+                    cell[row, col] = new Cell(new Rectangle(col * 10, row * 10, 10, 10), true, Color.Green, Color.Brown);
                 }
             }
         }
 
         public void Update()
         {
-            //cell.Update();
+            foreach (var i in cell)
+            {
+                i.Update();
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            foreach(var i in cell)
+            foreach (var i in cell)
             {
-                if(i != null)
-                    i.Draw(spriteBatch);
+                i.Draw(spriteBatch);
             }
         }
     }
