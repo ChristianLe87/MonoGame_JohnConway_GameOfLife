600bda0e2570d273a85b628001ca3b156781849c
Christian <christianle87@icloud.com>
2020-05-03 16:22:39 +0200

    Add macOS project

---
+++ macOS/Main.cs
@@ -0,0 +1,16 @@
+﻿using AppKit;
+using Shared;
+
+namespace macOS
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new MyGame())
+            {
+                game.Run();
+            }
+        }
+    }
+}
